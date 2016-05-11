e8bd3a5afd09db77f0cf60b78e6581bd0c664feb
BoSunesen <BoSunesen@users.noreply.github.com>
2016-05-12 00:33:02 +0200

    Move init code

--- init.go
+++ init.go
@@ -1,15 +1,9 @@
 package pointsofinterestlauncher
 
 import (
-	"github.com/BoSunesen/pointsofinterest/webapi/handlers"
-	"net/http"
+	"github.com/BoSunesen/pointsofinterest/webapi/initialization"
 )
 
 func init() {
-	const pingUrl string = "/ping/"
-	http.Handle(pingUrl, handlers.LoggingDecorator{handlers.PingHandler{}, "Ping"})
-	http.Handle("/poi/", handlers.LoggingDecorator{handlers.PoiHandler{}, "POI"})
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, pingUrl, http.StatusFound)
-	})
+	initialization.Initialize()
 }
