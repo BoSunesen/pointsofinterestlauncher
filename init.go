9a8817b5eb1128c18e3553f6149458223905a09d
BoSunesen <BoSunesen@users.noreply.github.com>
2016-05-16 19:03:48 +0200

    Inject dependencies to App Engine

--- init.go
+++ init.go
@@ -1,42 +1,66 @@
 package pointsofinterestlauncher
 
 import (
+	"github.com/BoSunesen/pointsofinterest/webapi/factories"
 	"github.com/BoSunesen/pointsofinterest/webapi/initialization"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/delay"
 	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/urlfetch"
 	"net/http"
 )
 
 func init() {
-	initialization.Initialize(AppEngineLog{})
+	initialization.Initialize(AppEngineLog{}, AppEngineContextFactory{}, AppEngineClientFactory{}, AppengineWorkerFactory{})
+}
+
+//TODO Move stuff out of init.go
+type AppengineWorkerFactory struct{}
+
+func (f AppengineWorkerFactory) CreateBackgroundWorker(key string, workFunction func(context.Context)) factories.BackgroundWorker {
+	delayedFunction := delay.Func(key, workFunction)
+	return AppEngineBackgroundWorker{delayedFunction: delayedFunction}
+}
+
+type AppEngineBackgroundWorker struct {
+	delayedFunction *delay.Function
+}
+
+func (worker AppEngineBackgroundWorker) DoWork(r *http.Request) error {
+	return worker.delayedFunction.Call(appengine.NewContext(r))
+}
+
+type AppEngineContextFactory struct{}
+
+func (f AppEngineContextFactory) CreateContext(r *http.Request) context.Context {
+	return appengine.NewContext(r)
+}
+
+type AppEngineClientFactory struct{}
+
+func (f AppEngineClientFactory) CreateClient(ctx context.Context) *http.Client {
+	return urlfetch.Client(ctx)
 }
 
 type AppEngineLog struct{}
 
-func (l AppEngineLog) Debugf(r *http.Request, format string, v ...interface{}) {
-	log.Debugf(GetContext(r), format, v...)
+func (l AppEngineLog) Debugf(ctx context.Context, format string, v ...interface{}) {
+	log.Debugf(ctx, format, v...)
 }
 
-func (l AppEngineLog) Infof(r *http.Request, format string, v ...interface{}) {
-	log.Infof(GetContext(r), format, v...)
+func (l AppEngineLog) Infof(ctx context.Context, format string, v ...interface{}) {
+	log.Infof(ctx, format, v...)
 }
 
-func (l AppEngineLog) Warningf(r *http.Request, format string, v ...interface{}) {
-	log.Warningf(GetContext(r), format, v...)
+func (l AppEngineLog) Warningf(ctx context.Context, format string, v ...interface{}) {
+	log.Warningf(ctx, format, v...)
 }
 
-func (l AppEngineLog) Errorf(r *http.Request, format string, v ...interface{}) {
-	log.Errorf(GetContext(r), format, v...)
+func (l AppEngineLog) Errorf(ctx context.Context, format string, v ...interface{}) {
+	log.Errorf(ctx, format, v...)
 }
 
-func (l AppEngineLog) Criticalf(r *http.Request, format string, v ...interface{}) {
-	log.Criticalf(GetContext(r), format, v...)
-}
-
-func GetContext(r *http.Request) context.Context {
-	if r == nil {
-		return appengine.BackgroundContext()
-	}
-	return appengine.NewContext(r)
+func (l AppEngineLog) Criticalf(ctx context.Context, format string, v ...interface{}) {
+	log.Criticalf(ctx, format, v...)
 }
